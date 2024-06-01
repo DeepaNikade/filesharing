e0792d5c8ff5587db172cf53c2d1f32abe6cb42c
Deepa <nikadedeepa01@gmail.com>
2024-06-01 23:33:26 +0530

    files edit

--- controllers/file.js
+++ controllers/file.js
@@ -1,17 +1,23 @@
 const multer = require("multer");
 const path = require("path");
+const {v4:uuidv4}=require("uuid");
 
 const uploaddirectorypath = path.join(__dirname, "..", "files");
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, uploaddirectorypath);
   },
+  filename:(req,file,cb)=>{
+    // const filename="resume"+path.extname(file.originalname);
+    const filename=uuidv4() + file.originalname;
+    cb(null,filename);
+  }
 });
 
 //middleware
 const upload = multer({
   storage: storage,
-}).array("dummy", 10);
+}).single("dummy");
 
 const uploadFile = async (req, res) => {
   upload(req, res, (error) => {
